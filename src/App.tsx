7e1ca7901fc0f3ef384d3a3b94c6cd82d334399e
Anshuman Komawar <anshumankomawar@gmail.com>
2024-12-13 22:06:05 -0800

    feat: add sidebar and macos inset

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import "./App.css";
 import Tiptap from "@/components/tiptap/Tiptap";
 import { CommandDialogDemo } from "./components/command/command";
+import { AppSidebar } from "@/components/sidebar/sidebar";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 function App() {
 	return (
-		<div>
-			<CommandDialogDemo />
-			<div className="h-screen w-full flex flex-col ">
-				<div className="h-7 flex-0 px-2"></div>
-				<div className="overflow-hidden h-full">
-					<Tiptap />
-				</div>
-			</div>
-		</div>
+		<SidebarProvider
+			style={
+				{
+					"--sidebar-width": "19rem",
+				} as React.CSSProperties
+			}
+		>
+			<AppSidebar />
+			<SidebarInset>
+				<header className="flex h-10 shrink-0 items-center gap-2 px-4">
+					<SidebarTrigger className="ml-16" />
+					<Separator orientation="vertical" className="mr-2 h-4" />
+				</header>
+				<Tiptap />
+				<CommandDialogDemo />
+			</SidebarInset>
+		</SidebarProvider>
 	);
 }
 
