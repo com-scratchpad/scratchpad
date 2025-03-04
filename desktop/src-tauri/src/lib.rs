825c51a50eac1789ec21366eda6c8ce7fb57cb8c
Ujwal Jain <reachujwal@gmail.com>
2025-03-03 21:38:42 -0800

    Integrate useDocumentStore().addDocument() with plugin_fs

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -13,10 +13,9 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_fs::init())
         .plugin(titlebar::init())
         .invoke_handler(tauri::generate_handler![greet, editor::save])
         .setup(|app| {
--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -9,12 +9,14 @@ import {
 } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
 import { MoreHorizontal } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import clsx from "clsx";
 import { SaveButton } from "./components/save/SaveButton";
 import { ModeToggle } from "./providers/theme/toggle";
 import { SearchPanel } from "@/components/command/SearchPanel";
 import useEditorStore from "@/stores/editorStore";
+import { initStore } from "./lib/stronghold";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
 	const [isOpen, setIsOpen] = useState(false);
@@ -22,6 +24,8 @@ function App() {
 		documentTitle,
 		documentContent,
 	} = useEditorStore();
+
+    useEffect(()=> {initStore()}, [])
 
 	return (
 		<SidebarProvider
@@ -33,6 +37,7 @@ function App() {
 				} as React.CSSProperties
 			}
 		>
+            <Toaster />
 			<AppSidebar />
 			<SidebarInset>
 				<header
