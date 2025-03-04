11c022c7c400d5ce3b772d2fc5b04c14e87a63c0
Ujwal Jain <reachujwal@gmail.com>
2025-03-03 19:56:56 -0800

    Enable add document to $APPDATA directory

--- desktop/src/stores/documentStore.tsx
+++ desktop/src/stores/documentStore.tsx
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { BaseDirectory, exists, writeTextFile } from "@tauri-apps/plugin-fs";
+import { BaseDirectory, exists, writeTextFile, mkdir } from "@tauri-apps/plugin-fs";
 import { getUserId } from "@/lib/stronghold";
 import { join } from "@tauri-apps/api/path";
 
@@ -19,21 +19,23 @@ const DOCUMENTS_DIR = "documents"
 export const useDocumentStore = create<DocumentListState>((set) => ({
     documents: [],
     addDocument: async (document: DocumentInfo) => {
-        //try {
-            //const userId = await getUserId() ?? "null";
-            //if (userId == "null") {
-                //console.error("Failed to add documetn because userId cannot be found");
-            //}
-            //const filename = await join(userId, DOCUMENTS_DIR, document.id)
-            //const fileExists = await exists(filename, { baseDir: BaseDirectory.AppData })
-            //if (fileExists) {
-                //console.error("File already exists")
-            //}
-            //await writeTextFile(filename, document.content, { baseDir: BaseDirectory.AppData });
-        //} catch (e) {
-            //console.error("Failed to add document: ", e);
-            //set((state) => state)
-        //}
+        try {
+            const userId = await getUserId() ?? "null";
+            if (userId == "null") {
+                console.error("Failed to add documetn because userId cannot be found");
+            }
+            const documentsDir = await join(userId, DOCUMENTS_DIR)
+            await mkdir(documentsDir, {baseDir: BaseDirectory.AppData, recursive: true})
+            const filename = await join(userId, DOCUMENTS_DIR, document.id)
+            const fileExists = await exists(filename, { baseDir: BaseDirectory.AppData })
+            if (fileExists) {
+                console.error("File already exists")
+            }
+            await writeTextFile(filename, document.content, { baseDir: BaseDirectory.AppData });
+        } catch (e) {
+            console.error("Failed to add document: ", e);
+            set((state) => state)
+        }
         set((state) => {
             console.log("updating doucments:", [ ...state.documents, document ])
             return {
